ebc736d0a0322fd7a103f334f10200668eb01af9
Ethan Bourguigneau <ethan.bourguigneau@etu.univ-grenoble-alpes.fr>
2023-01-05 16:36:01 +0100

    liste rdv in progress: chargement des rdv en fonction de l'etat de la Dde, affichage Date en fonction devis/facture

--- public/classes/DevisDAO.php
+++ public/classes/DevisDAO.php
@@ -2,12 +2,12 @@
 
 class DevisDAO extends DAO
 {
-    public function getOne(int|string $id): array|object
+    public function getOne(int|string $id): Devis
     {
         $stmt = $this->pdo->prepare("SELECT * FROM Devis WHERE NoDevis = ?");
         $stmt->execute([$id]);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        return new Devis($row['NoDevis'], $row['NoFacture'], $row['NumDde'], $row['DateDevis'], $row['PrixEstimer'], $row['TauxTva'], $row['estimation_fin']);
+        return new Devis($row['NoDevis'], $row['NoFacture'], $row['NumDde'], $row['DateDevis'], $row['PrixEstimer'], $row['TauxTVA'], $row['estimation_fin']);
     }
 
     public function getAll(): array
@@ -16,14 +16,14 @@ class DevisDAO extends DAO
         $res = [];
         $stmt = $this->pdo->query("SELECT * FROM Devis");
         foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row)
-            $res[] = new Devis($row['NoDevis'], $row['NoFacture'], $row['NumDde'], $row['DateDevis'], $row['PrixEstimer'], $row['TauxTva'], $row['estimation_fin']);
+            $res[] = new Devis($row['NoDevis'], $row['NoFacture'], $row['NumDde'], $row['DateDevis'], $row['PrixEstimer'], $row['TauxTVA'], $row['estimation_fin']);
         return $res;
 
     }
 
     public function insert(object $obj): int
     {
-        $stmt = $this->pdo->prepare("INSERT INTO Devis (NoFacture,NumDde, DateDevis,PrixEstimer,TauxTva,estimation_fin)"
+        $stmt = $this->pdo->prepare("INSERT INTO Devis (NoFacture,NumDde, DateDevis,PrixEstimer,TauxTVA,estimation_fin)"
             . " VALUES (?,?,?,?,?,?)");
         $res = $stmt->execute([$obj->getNoFacture(), $obj->getNumDde(), $obj->getDateDevis(), $obj->getPrixEstimer(),$obj->getTauxTva(),$obj->getEstimationFin()]);
         $obj->setNoDevis($this->pdo->lastInsertId());
